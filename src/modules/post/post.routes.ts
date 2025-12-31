e91f36f07da70f75d01b2ecef38ce56e694ecfd0
unknown <abuhasanxt@gmail.com>
2025-12-31 21:47:47 +0600

    Authentication Middleware

--- src/modules/post/post.routes.ts
+++ src/modules/post/post.routes.ts
@@ -1,11 +1,22 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { postController } from "./post.controller";
+import {auth as betterAuth} from "../../lib/auth"
 
 const router = express.Router();
 
+const auth =(...roles:any)=>{
+    return async (req:Request,res:Response,next:NextFunction)=>{
+        //get user season 
+const season=await betterAuth.api.getSession({
+    headers:req.headers as any
+})
 
+console.log(season);
+        next()
+    }
+}
 router.post(
-    "/",postController.createPost
+    "/",auth("ADMIN","USER"), postController.createPost
 )
 
 router.get("/",postController.getAllPost )
