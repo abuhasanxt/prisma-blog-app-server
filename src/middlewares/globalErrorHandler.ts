f84c98839b0119187797acb2ccf989f230c7ed85
unknown <abuhasanxt@gmail.com>
2026-01-12 23:19:42 +0600

    Dealing with Prisma Client Unknown & Initialization Errors

--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -22,14 +22,38 @@ function errorHandler(
       (statusCode = 400),
         (errorMessage =
           "An operation failed because it depends on one or more records that were required but not found");
-    } 
-    else if (err.code === "P2002") {
+    } else if (err.code === "P2002") {
       (statusCode = 400), (errorMessage = "Duplicate kye error");
-    }
-    else if (err.code === "P2003") {
+    } else if (err.code === "P2003") {
       (statusCode = 400), (errorMessage = "Foreign  kye constraint failed");
     }
   }
+  //PrismaClientUnknownRequestError
+  else if (err instanceof Prisma.PrismaClientUnknownRequestError) {
+    (statusCode = 500),
+      (errorMessage = "Error occurred during query execution");
+  }
+  //PrismaClientRustPanicError
+  else if (err instanceof Prisma.PrismaClientRustPanicError) {
+    (statusCode = 400),
+      (errorMessage =
+        "The error message usually provides details about where in the internal Rust code the panic occurred, which often points to a bug in Prisma's engines rather than a user configuration error (unless the configuration is fundamentally invalid");
+  }
+  //PrismaClientInitializationError
+  else if (err instanceof Prisma.PrismaClientInitializationError) {
+    if (err.errorCode === "P1000") {
+      (statusCode = 401),
+        (errorMessage =
+          "Authentication failed . please check your credentials !");
+    }
+
+    else if (err.errorCode==="P1001") {
+          (statusCode = 400),
+        (errorMessage =
+          "Can't reach database server !");
+    }
+  }
+
   res.status(statusCode);
   res.json({
     message: errorMessage,
